98a52a234d7b11d1fc23289b8573ddd3d5648a3c
Sebastianuribe1212 <sebastian.uribea@udea.edu.co>
2020-10-22 09:10:41 -0500

    implementacion prueba mala

--- main.cpp
+++ main.cpp
@@ -3,10 +3,28 @@
  */
 #include <iostream>
 
+//Variables globales y macros
+#define TAM (int)10
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char a[TAM] = "AB11CD44";
+
+
+    for(int var = 0; var < TAM; ++var){
+        cout << a[var]<<endl;
+    }
     return 0;
+
+
+// sacar numeros
+cout << "Imprimir solo los numeros de la cadena original"<<endl;
+
+for(int var =0; var < TAM; ++var){
+    if(a[var]>= '0' or a[var] <= '9'){
+        cout << a[var]<<endl;
+    }
+
+   }
 }
