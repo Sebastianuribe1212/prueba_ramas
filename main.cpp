766815416cfcaa37fee1916b5e2eab3df55b84e3
Sebastianuribe1212 <sebastian.uribea@udea.edu.co>
2020-10-22 09:19:14 -0500

    imprime esa monda

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 #include <iostream>
 
 //Variables globales y macros
-#define TAM (int)10
+#define TAM (int)50
 using namespace std;
 
 int main()
 {
-    char a[TAM] = "AB11CD44";
+    char a[TAM] = "AB11CD44EF5X5";
 
 
     for(int var = 0; var < TAM; ++var){
+        if(a[var] != '\0'){
         cout << a[var]<<endl;
+        }
     }
     return 0;
 
@@ -22,7 +24,7 @@ int main()
 cout << "Imprimir solo los numeros de la cadena original"<<endl;
 
 for(int var =0; var < TAM; ++var){
-    if(a[var]>= '0' or a[var] <= '9'){
+    if(a[var]>= '0' && a[var] <= '9'){
         cout << a[var]<<endl;
     }
 
